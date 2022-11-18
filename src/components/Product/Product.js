80d0631377ef823ab88ca9770770687a6829639f
ankushguptamay <ankushgupta9675@gmail.com>
2022-11-18 22:14:25 +0530

    some change

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -12,7 +12,29 @@ export default function Product(props){
                     <h5 className="price">{props.price}</h5>
                     <p className="massage">{props.massage}</p>
                     <button className="btn btn-outline-warning mx-2" type="submit">Delete</button>
-                    <button className="btn btn-outline-warning mx-2" type="submit">UpDate</button>
+                    <button className="btn btn-outline-warning mx-2" type="submit">Update</button>
+                </div>
+            </div>
+
+            <div className="card m-3 text-center">
+                <img src="./download.jpg" className="card-img-top" alt="not available"/>
+                <div className="card-body">
+                    <h5 className="productName">{props.productName}</h5>
+                    <h5 className="price">{props.price}</h5>
+                    <p className="massage">{props.massage}</p>
+                    <button className="btn btn-outline-warning mx-2" type="submit">Delete</button>
+                    <button className="btn btn-outline-warning mx-2" type="submit">Update</button>
+                </div>
+            </div>
+
+            <div className="card m-3 text-center">
+                <img src="./download.jpg" className="card-img-top" alt="not available"/>
+                <div className="card-body">
+                    <h5 className="productName">{props.productName}</h5>
+                    <h5 className="price">{props.price}</h5>
+                    <p className="massage">{props.massage}</p>
+                    <button className="btn btn-outline-warning mx-2" type="submit">Delete</button>
+                    <button className="btn btn-outline-warning mx-2" type="submit">Update</button>
                 </div>
             </div>
         </div>
